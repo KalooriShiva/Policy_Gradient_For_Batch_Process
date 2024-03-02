dfc18f638a9f949a60ef393c33db4651009c743e
Likithkumar572002 <161055315+Likithkumar572002@users.noreply.github.com>
2024-03-02 16:43:38 +0530

    Add files via upload
    First Files

---
+++ agents_pg.py
@@ -0,0 +1,92 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import Model
+import tensorflow_probability as tfp
+
+
+class PGAgent:
+    
+    def __init__(self, environment, learning_rate=1e-5, decay_rate=1e-6, discount_factor=1, epsilon=0.05,  nn_arch=[400, 300, 200]):
+        self.env = environment
+        self.discount_factor = discount_factor
+        self.epsilon = epsilon
+        self.P = self._get_model(nn_arch)
+        self.opt = tf.keras.optimizers.legacy.RMSprop(learning_rate=learning_rate, decay=decay_rate)
+        self.action_memory = []
+        self.reward_memory = []
+        self.state_memory = []
+        
+        
+    def _get_model(self, nn_arch):
+        model = tf.keras.models.Sequential()
+        model.add(tf.keras.layers.Dense(nn_arch[0], input_shape=(3,), activation="relu"))
+        for num_neurons in nn_arch[1:]:
+            model.add(tf.keras.layers.Dense(num_neurons, activation="relu"))
+        model.add(tf.keras.layers.Dense(self.env.num_j_temp, activation="softmax"))
+        model.build()
+        return model
+    
+    
+    def choose_action_training(self,state):
+        prob = self.P(np.array([[state[0,0], state[1, 0], state[2, 0]]]))
+        dist = tfp.distributions.Categorical(probs=prob,dtype=tf.float32)
+        action = dist.sample()
+        self.action_memory.append(action)
+        # print(action)
+        return int(action.numpy())
+    
+    def choose_action(self, state):
+        action_index = tf.argmax(self.P(tf.reshape(state, (1, -1)))[0])
+        return self.env.tj_list[action_index]
+    
+    
+    def calc_loss(self,prob,action,returns):
+        dist = tfp.distributions.Categorical(probs=prob, dtype=tf.float32)
+        log_prob = dist.log_prob((action))
+        loss = -log_prob*returns
+        return loss
+    
+    def update_p_weights(self):
+        sum_reward = 0
+        discnt_rewards = []
+        self.reward_memory.reverse()
+        for r in self.reward_memory:
+            sum_reward = r + self.discount_factor*sum_reward
+            discnt_rewards.append(sum_reward)
+        discnt_rewards.reverse()
+            
+        for state,action,returns in zip(self.state_memory,self.action_memory,discnt_rewards):
+            with tf.GradientTape() as tape:
+                prob = self.P(np.reshape(state, (1, -1)))
+                loss = self.calc_loss(prob,action,returns)
+                grads = tape.gradient(loss,self.P.trainable_variables)
+                self.opt.apply_gradients(zip(grads,self.P.trainable_variables))
+        #If done with updating empty all memories of this episode
+        self.reward_memory = []
+        self.action_memory = []
+        self.state_memory = []
+    
+    
+    def train(self, num_episodes):
+        iter_num = 0
+        episode_versus_reward = np.zeros((num_episodes, 2))
+        for episode_index in range(num_episodes):
+            # initialize markov chain with initial state
+            state = self.env.reset()
+            cumulative_reward = 0
+            while not self.env.done:
+                action_index = self.choose_action_training(state)
+                action = self.env.tj_list[action_index]
+                # executing action, observing reward and next state to store experience in tuple
+                next_state, reward, done, info = self.env.step(action)
+                cumulative_reward += (self.discount_factor ** iter_num) * reward
+                # store experience in replay memory
+                self.state_memory.append(state)
+                self.reward_memory.append(reward)
+                iter_num += 1
+                state = next_state
+            self.update_p_weights()
+            if episode_index % 10000 == 0: self.P.save(f"P_{episode_index}.h5")
+            if episode_index % 100 == 0: print(f"[episodes]: {episode_index}")
+            episode_versus_reward[episode_index] = np.array([episode_index, cumulative_reward])
+        return episode_versus_reward
---
+++ main_pg.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from reactor_environments import Environment
+from agents_pg import PGAgent
+# import tensorflow as tf
+import matplotlib.pyplot as plt
+import time
+    
+    
+if __name__ == "__main__":
+    env = Environment(timesteps=40, num_j_temp=40)
+    agent = PGAgent(learning_rate=1e-6, decay_rate=1e-7, environment=env, nn_arch=[400, 300, 200])
+    #agent.P = tf.keras.models.load_model(r"C:\Users\Dr Nabil\Downloads\Policy gradient\Pg_final\P_network.h5")
+    env.testing = True
+    start_time = time.perf_counter()
+    episode_versus_reward = agent.train(100000)
+    cpu_time = time.perf_counter() - start_time
+    agent.P.save("P_network.h5")
+    state_arr = np.zeros_like(env.time_list)
+    conc_arr = np.zeros_like(env.time_list)
+    action_arr = np.zeros_like(env.time_list)
+    reward_arr = np.zeros_like(env.time_list)
+    episode_vs_reward_df = pd.DataFrame({"Episodes": episode_versus_reward[:, 0], "Reward": episode_versus_reward[:, 1]})
+    env.curr_state = np.vstack([0, 298, 0.6])
+    state = env.curr_state
+    for i in range(env.n_tf):
+        state_arr[i] = state[env.T, 0]
+        conc_arr[i] = state[env.Ca, 0]
+        action_arr[i] = agent.choose_action(state)
+        #reward_computation
+        next_state, reward, done, info = env.step(action_arr[i])
+        reward_arr[i] = reward
+        state = next_state
+    df = pd.DataFrame({"Temperature": state_arr, "Time": env.time_list, "Reference": env.Tref, "Jacket Temperature": action_arr, "Concentration [A]": conc_arr, "Reward": reward_arr})
+    df.to_excel("PG.xlsx")
+    sns.set_theme()
+    sns.lineplot(
+        data=df,
+        x="Time",
+        y="Temperature",
+        label="Reactor Temperature",
+    )
+    sns.lineplot(
+        data=df,
+        x="Time",
+        y="Reference",
+        legend="full",
+        label="Reference Temperature",
+    )
+    plt.show()
+    sns.lineplot(
+        data=df,
+        x="Time",
+        y="Jacket Temperature",
+        legend="full",
+        label="Action",
+    )
+    plt.show()
+    # concentration plot
+    sns.lineplot(data=df, x="Time", y="Concentration [A]")
+    plt.show()
+    window_size = 100
+    rolling_avg = pd.Series(episode_versus_reward[:,1]).rolling(window=window_size, min_periods=1).mean()
+    data = pd.DataFrame({'Episode': range(1, episode_versus_reward.shape[0] + 1),
+                         'Reward':episode_versus_reward[:,1] ,
+                         'Rolling Average': rolling_avg})
+    sns.set_style("darkgrid")
+    sns.lineplot(data=data, x='Episode', y = 'Reward', label='Episode Reward', color='blue')
+    sns.lineplot(data=data, x='Episode', y = 'Rolling Average', label='Rolling Average', color='red')
+    # sns.lineplot(episode_vs_reward_df, x="Episodes", y="Reward")
+    plt.show()
+    # control signal plot
+    # sns.lineplot(x=env.time_list, y=action_arr, drawstyle='steps-pre', label="Jacket Temperature")
+    # episode versus reward plot
+    # sns.lineplot(episode_vs_reward_df, x="Episodes", y="Reward")
+    # plt.show()
+    MAE = np.sum(np.abs(state_arr-env.Tref))/len(state_arr)
+    RMSE = (np.sum((state_arr-env.Tref)**2)/len(state_arr))**0.5
+    days = int(cpu_time // 86400)
+    hrs = int((cpu_time -  86400 * days)// 3600)
+    mins = int((cpu_time - 3600 * hrs - 86400 * days) // 60)
+    seconds = int((cpu_time - 60 * mins - 3600 * hrs - 86400 * days) // 1)
+    print(f"{days = }\n{hrs = }\n{mins = }\n{seconds = }")  
